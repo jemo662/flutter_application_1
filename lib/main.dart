129bef2d8bb8944341d3d791448b5e8f22785014
KagemaNjoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2023-08-31 16:06:21 +0300

    Project setup

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// sample app
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Home')),
+        body: const Center(
+          child: Text('Home'),
+        ));
+  }
+}
